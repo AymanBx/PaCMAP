eef94c525a9ecc9c8d2ae78fafed302204759868
AymanBx <ayman_sandouk@uri.edu>
2025-11-05 19:56:07 +0000

    Run.py: Accept arguments \(dataset, reducer\), load data,  run knn function, trustworthiness function and plot

--- run.py
+++ run.py
@@ -2,69 +2,73 @@ import sys
 import pacmap
 import numpy as np
 import matplotlib.pyplot as plt
+from DataLoader import DatasetLoader
+from knn import run_knn
+from trustworthiness_eval import pacmap_embedding
+from trustworthiness_eval import pca_embedding
+from trustworthiness_eval import trustworthiness
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 
 
-datapath = sys.argv[1]
+dataset = sys.argv[1] or input("Which dataset would you like to test with?\n")
+reducer_type = sys.argv[2] or "pacmap"
+eval_metric = sys.argv[3] or None
+
+# if type(dataset) != list
+match dataset:
+    case 'coil20':
+       loader = DatasetLoader('dataset',
+                dataset_path='datasets/coil-20'
+                )
+    case 'coil20-npy':
+        loader = DatasetLoader('npy',
+                data_path='datasets/coil20/coil_20.npy',
+                labels_path='datasets/coil20/coil_20_labels.npy'
+                )
+    case 'mnist':
+        loader = DatasetLoader('dataset',
+                training_images='datasets/MNIST/train-images.idx3-ubyte',
+                training_labels='datasets/MNIST/train-labels.idx1-ubyte',
+                test_images='datasets/MNIST/t10k-images.idx3-ubyte',
+                test_labels='datasets/MNIST/t10k-labels.idx1-ubyte'
+                )
+    case 'olivetti':
+        loader = DatasetLoader('npy', 
+                data_path='datasets/olivetti/olivetti_faces.npy', 
+                labels_path='datasets/olivetti/olivetti_faces_target.npy'
+                )
+
+match reducer_type:
+    case 'pacmap': reducer = pacmap_embedding
+    case 'pca': reducer = pca_embedding
+
+# loading preprocessed 
+(X_train, y_train), (X_test, y_test) = loader.load_data()
+
+# Get trustworthiness
+run_trustworthiness(dataset, X_train, y_train, X_test, y_test, embedding_func=reducer)
 
 
-# loading preprocessed coil_20 dataset
-# you can change it with any dataset that is in the ndarray format, with the shape (N, D)
-# where N is the number of samples and D is the dimension of each sample
-X = np.load("./datasets/coil_20.npy", allow_pickle=True)
-y = np.load("./datasets/coil_20_labels.npy", allow_pickle=True)
-print("X: ", X.shape)
-print("y: ", y.shape)
-
-X = X.reshape(X.shape[0], -1)
-print("X: ", X.shape)
-
-
-# Can't plot images 
-# visualize the data as is..?
-# fig, ax = plt.subplots(1, 1, figsize=(10, 10))
-# ax.scatter(X[:, 0], X[:, 1], cmap="Spectral", c=y, s=0.6)
-# plt.savefig('before.png')
-
+# run knn
 print("KNN Before:")
-# Get KNN before DR applies
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=1, stratify=y # stratify ensures proportions are the same before and after split.
-)
-
-for k in [1, 3, 5, 7, 9]:
-    knn = KNeighborsClassifier(n_neighbors=k)
-    knn.fit(X_train, y_train)
-    acc = knn.score(X_test, y_test)
-    print(f"k={k}: {acc:.4f}")
+run_knn(dataset, X_train, y_train, X_test, y_test)
 
 
-# initializing the pacmap instance
-# n_components: the number of dimension of the output. Default to 2.
-# n_neighbors: the number of neighbors considered in the k-Nearest Neighbor graph.
-# MN_ratio: the ratio of the number of mid-near pairs to the number of neighbors, n_MN = \lfloor n_neighbors * MN_ratio
-# FP_ratio: the ratio of the number of further pairs to the number of neighbors, n_FP = \lfloor n_neighbors * FP_ratio
-# Setting n_neighbors to "None" leads to an automatic choice shown below in "parameter" section
-reducer = pacmap.PaCMAP(n_components=2, n_neighbors=10, MN_ratio=0.5, FP_ratio=2.0) 
+# Flatten if necessary
+X_train = np.array(X_train)
+X_test = np.array(X_test)
+X_train_flat = X_train.reshape(X_train.shape[0], -1)
+X_test_flat = X_test.reshape(X_test.shape[0], -1)
 
-X_combined = np.vstack([X_train, X_test])
+
+# initializing the DR instance
+X_train_embedded = reducer(X_train_flat)
+X_test_embedded = reducer(X_test_flat)
 
 print("KNN After:")
-# fit the data (The index of transformed data corresponds to the index of the original data)
-X_transformed = reducer.fit_transform(X_combined, init="pca")
-print("X: ", X_transformed.shape)
+run_knn(dataset, X_train_embedded, y_train, X_test_embedded, y_test)
 
-
-# Get KNN after the 
-X_train = X_transformed[0:len(X_train)]
-X_test = X_transformed[len(X_train):]
-
-for k in [1, 3, 5, 7, 9]:
-    knn = KNeighborsClassifier(n_neighbors=k)
-    knn.fit(X_train, y_train)
-    acc = knn.score(X_test, y_test)
-    print(f"k={k}: {acc:.4f}")
 
 
 # visualize the reducer
