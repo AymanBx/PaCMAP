691abd328ab7e25e2dfb45390948dd3520268eae
AymanBx <ayman_sandouk@uri.edu>
2025-11-25 22:06:01 +0000

    Embedding: Accept dimention as argument

--- embedding.py
+++ embedding.py
@@ -1,15 +1,16 @@
 from pacmap import PaCMAP
 from sklearn.decomposition import PCA
 
-def pacmap_embedding(X_train, X_test):
-    pacmap_reducer = PaCMAP(random_state=42, n_components=3)
+
+def pacmap_embedding(X_train, X_test, dimentions):
+    pacmap_reducer = PaCMAP(random_state=42, n_components=dimentions)
     X_train_embedded = pacmap_reducer.fit_transform(X_train)
     X_test_embedded  = pacmap_reducer.transform(X_test, X_train)
     return X_train_embedded, X_test_embedded
 
-def pca_embedding(X_train, X_test):
+def pca_embedding(X_train, X_test, dimentions):
     # Initialize PCA and fit/transform on the training data
-    pca = PCA(n_components=2)
+    pca = PCA(n_components=dimentions)
     X_train_embedded = pca.fit_transform(X_train)
     # Transform the test data using the same PCA model
     X_test_embedded = pca.transform(X_test)
