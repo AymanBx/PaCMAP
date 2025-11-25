c93a35a0fc0615972a8ed6c76fca37b5eb7f839b
AymanBx <ayman_sandouk@uri.edu>
2025-11-25 22:09:33 +0000

    Run: Setup log file and output to it. Accept dimention as argument. Have the ability to load datasets unsplit to be able to handle splitting later

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import numpy as np
 from knn import run_knn
@@ -8,16 +9,24 @@ from DataLoader import DatasetLoader
 from continuity import run_continuity
 from embedding import pacmap_embedding
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
 from trustworthiness_eval import run_trustworthiness
 
 ## Set initial flags
 knn = trust = mrre = continuity = plot = False
+split = False # Do we want to apply the DR technique before or after splitting
 
 ## Read user arguments 
 dataset = sys.argv[1] if len(sys.argv) > 1 else input("Which dataset would you like to test with? Options: coil20 - coil20-npy - mnist - olivetti\n")
 reducer_type = sys.argv[2] if len(sys.argv) > 2 else "pacmap"
 eval_metric = sys.argv[3] if len(sys.argv) > 3 else "all"
 plot = True if len(sys.argv) > 4 and sys.argv[4] == 'plot' else False
+dimention = int(sys.argv[5] if len(sys.argv) > 5 else 3) # Change to select the new dimention size after DR
+
+## Setup log file
+os.chdir(f"results/{reducer_type}/{dimention}-dim")
+log = open(f"{dataset}_{reducer_type}-{dimention}.log", 'w')
+os.chdir("../../..")
 
 # if type(dataset) != list
 match dataset:
@@ -56,60 +65,69 @@ match eval_metric:
     case 'continuity': continuity = True
     case 'all': knn = trust = mrre = continuity = True
 
-## loading preprocessed 
-(X_train, y_train), (X_test, y_test) = loader.load_data()
+## loading preprocessed
+if split:
+    (X_train, y_train), (X_test, y_test) = loader.load_data(split)
+else:
+    X, y = loader.load_data(split)
+
+    # We move the splitting to here to control how we apply the DR technique
+    X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2, stratify=y if len(np.unique(y)) > 1 else None, random_state=1)
 
 ## Flatten data
 X_train = np.array(X_train)
 X_test = np.array(X_test)
-print("X_train shape:", X_train.shape)
-print("X_test shape:", X_test.shape)
+print("X_train shape:", X_train.shape, file=log)
+print("X_test shape:", X_test.shape, file=log)
 X_train_flat = X_train.reshape(X_train.shape[0], -1)
 X_test_flat = X_test.reshape(X_test.shape[0], -1)
-print("X_train flattened shape:", X_train_flat.shape)
-print("X_test flattened shape:", X_test_flat.shape)
-print()
+print("X_train flattened shape:", X_train_flat.shape, file=log)
+print("X_test flattened shape:", X_test_flat.shape, file=log)
+print(file=log)
 
 ## Apply DR technique
-print("Generating embeddings...")
-X_train_embedded, X_test_embedded = reducer(X_train_flat, X_test_flat)
-print("X_train embedded shape:", X_train_embedded.shape)
-print("X_test embedded shape:", X_test_embedded.shape)
-print()
+print("Generating embeddings...", file=log)
+X_train_embedded, X_test_embedded = reducer(X_train_flat, X_test_flat, dimention)
+print("X_train embedded shape:", X_train_embedded.shape, file=log)
+print("X_test embedded shape:", X_test_embedded.shape, file=log)
+print(file=log)
 
 ## Evaluation Metrics
 # run knn
 if knn:
-    print("Run KNN on ==> ", dataset)
-    print("KNN Before:")
-    run_knn(X_train_flat, y_train, X_test_flat, y_test)
+    print("Run KNN on ==> ", dataset, file=log)
+    print("KNN Before:", file=log)
+    run_knn(X_train_flat, y_train, X_test_flat, y_test, log)
 
-    print("KNN After:")
-    run_knn(X_train_embedded, y_train, X_test_embedded, y_test)
-    print()
+    print("KNN After:", file=log)
+    run_knn(X_train_embedded, y_train, X_test_embedded, y_test, log)
+    print(file=log)
 
 # Get trustworthiness
 if trust:
     if dataset == 'mnist':
-        print("Run Trustworthiness on ==> ", dataset)
+        print("Run Trustworthiness on ==> ", dataset, file=log)
         X_train_flat = X_train_flat.astype('float32')
         X_train_embedded = X_train_embedded.astype('float32')
         X_test_flat = X_test_flat.astype('float32')
         X_test_embedded = X_test_embedded.astype('float32')
-        run_trustworthiness(X_train_flat, X_train_embedded, X_test_flat, X_test_embedded, embedding_func=reducer)
+        run_trustworthiness(X_train_flat, X_train_embedded, X_test_flat, X_test_embedded, embedding_func=reducer, log_file=log)
     else:
-        print("Run Trustworthiness on ==> ", dataset)
-        run_trustworthiness(X_train_flat, X_train_embedded, X_test_flat, X_test_embedded, embedding_func=reducer)
+        print("Run Trustworthiness on ==> ", dataset, file=log)
+        run_trustworthiness(X_train_flat, X_train_embedded, X_test_flat, X_test_embedded, embedding_func=reducer, log_file=log)
 
 if mrre:
-    run_mrre(X_train_flat, X_train_embedded)
+    run_mrre(X_train_flat, X_train_embedded, log)
 
 if continuity:
-    run_continuity(X_train_flat, X_train_embedded)
+    run_continuity(X_train_flat, X_train_embedded, log)
 
+log.close()
 
 # visualize the reducer
 if plot:
+    os.chdir(f"results/{reducer_type}/{dimention}-dim")
     fig, ax = plt.subplots(1, 1, figsize=(6, 6))
     ax.scatter(X_train_embedded[:, 0], X_train_embedded[:, 1], cmap="Spectral", c=y_train, s=0.6)
-    plt.savefig('after.png')
+    plt.savefig(f'{dataset}_{reducer_type}-{dimention}.png')
