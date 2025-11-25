fca8cb460a5d6b809417ded6c7d141eec3bf2a09
AymanBx <ayman_sandouk@uri.edu>
2025-11-25 02:43:58 +0000

    knn: flattening of the data happens in run.

--- knn.py
+++ knn.py
@@ -2,20 +2,10 @@ import numpy as np
 from DataLoader import DatasetLoader
 from sklearn.neighbors import KNeighborsClassifier
 
-def run_knn(db_name, X_train, y_train, X_test, y_test):
-    print("Run KNN on ==> ", db_name)
-    
-    # Flatten if necessary
-    x_train = np.array(X_train)
-    x_test = np.array(X_test)
-    x_train_flat = x_train.reshape(x_train.shape[0], -1)
-    x_test_flat = x_test.reshape(x_test.shape[0], -1)
-    
-    print("x_train flattened shape:", x_train_flat.shape)
-    print("x_test flattened shape:", x_test_flat.shape)
+def run_knn(X_train, y_train, X_test, y_test):
 
     for k in [1, 3, 5, 7, 9]:
         knn = KNeighborsClassifier(n_neighbors=k)
-        knn.fit(x_train_flat, y_train)
-        acc = knn.score(x_test_flat, y_test)
+        knn.fit(X_train, y_train)
+        acc = knn.score(X_test, y_test)
         print(f"k={k}: {acc:.4f}")
